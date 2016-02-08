b2cd7dd8adb2fc3c24c4239506457848208878d6
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-02-08 15:07:57 -0800

    Initial commit; passing first test. Login redirect.

---
+++ app/models/user.js
@@ -0,0 +1,38 @@
+var db = require('../config');
+/// bcrypt: ///
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+
+  initialize : function(){
+    //needed stuff?
+  }
+});
+
+module.exports = User;
+
+
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   clicks: function() {
+//     return this.hasMany(Click);
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+// module.exports = Link;
